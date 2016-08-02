0e6e417282690ebebb9360f41dbd97b95d98f873
Roman Ivanov <romazzz85zzz2001@gmail.com>
2016-08-02 16:30:20 +0300

    some work for adding pros and cons

--- assets/bundle.js
+++ assets/bundle.js
@@ -181,6 +181,21 @@ var main =
 	   templateUrl:"../partial-views/subject.html",
 	   controller: function (selectedService) { 
 	       this.subj = selectedService.getSelected();
+
+	       this.proCons;
+	       this.positive = () => {
+	         this.proCons = true;
+	       }
+
+	       this.negative = () => {
+	           this.proCons = false;
+	       }
+
+	       this.addArg = () => {
+	          this.proCons ? this.subj.pro.push(this.requestData) : this.subj.cons.push(this.requestData);
+	       }
+
+	    
 	   }
 	}
 
--- scripts/subject/subject.js
+++ scripts/subject/subject.js
@@ -2,6 +2,21 @@ var subject = {
    templateUrl:"../partial-views/subject.html",
    controller: function (selectedService) { 
        this.subj = selectedService.getSelected();
+
+       this.proCons;
+       this.positive = () => {
+         this.proCons = true;
+       }
+
+       this.negative = () => {
+           this.proCons = false;
+       }
+
+       this.addArg = () => {
+          this.proCons ? this.subj.pro.push(this.requestData) : this.subj.cons.push(this.requestData);
+       }
+
+    
    }
 }
 
