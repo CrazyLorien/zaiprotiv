2b40bcd2730f665ebe53a8b23313a5db61e645b0
hem1kuda <Qwerty54321>
2016-08-07 16:22:32 +0300

    added files

--- scripts/subject/subject.js
+++ scripts/subject/subject.js
@@ -1,41 +1,43 @@
 var config = require('../../config')
 
 var subject = {
-   templateUrl:"../partial-views/subject.html",
-   controller: function (selectedService, dataService, $timeout) {
-       this.subj = [] ; 
-       dataService.getById(config.url, selectedService.getSelected().id).then( (response) => {
-          var temp = response.data.filter(function(rw){ return rw.id == selectedService.getSelected().id });
-          this.subj = temp[0];
-       })
-       var self = this;
-       
-
-       this.addArg = () => {
-          this.argumentStatus ? this.subj.arguments.pro.push({
-                      "title": self.argumentTitle,
-                      "body":  self.argumentBody,
-                      "rang": "27854",
-                      "id": "2",
-                      "image_url": "",
-                       "isImportant" : true
-                      })
-                     : this.subj.arguments.cons.push({
-                      "title": self.argumentTitle,
-                      "body":  self.argumentBody,
-                      "rang": "27854",
-                      "id": "2",
-                      "image_url": "",
-                       "isImportant" : true
-                    }
-);
-        
-       }
+    templateUrl: "../partial-views/subject.html",
+    controller: function (selectedService, dataService, $timeout, $location) {
+        this.subj = [];
+        dataService.getById(config.url, selectedService.getSelected().id).then((response) => {
+            var temp = response.data.filter(function (rw) {
+                return rw.id == selectedService.getSelected().id
+            });
+            this.subj = temp[0];
+        });
+        var self = this;
 
 
+        this.addArg = () => {
+            this.argumentStatus ? this.subj.arguments.pro.push({
+                "title": self.argumentTitle,
+                "body": self.argumentBody,
+                "rang": "27854",
+                "id": "2",
+                "image_url": "",
+                "isImportant": true
+            })
+                : this.subj.arguments.cons.push({
+                    "title": self.argumentTitle,
+                    "body": self.argumentBody,
+                    "rang": "27854",
+                    "id": "2",
+                    "image_url": "",
+                    "isImportant": true
+                }
+            );
 
-    
-   }
-}
+        };
+
+        this.showResult = ()=> {
+            $location.path("/results/");
+        };
+    }
+};
 
 module.exports = subject;
