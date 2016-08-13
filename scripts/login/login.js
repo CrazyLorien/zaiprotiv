3704bb176dd90689b0024821ee9ca94f0b6dd989
roma <roma_85_2001@tut.by>
2016-08-13 17:05:48 +0300

    Add code for login auth - need back-end support

--- scripts/login/login.js
+++ scripts/login/login.js
@@ -1,11 +1,19 @@
 var login = {
     templateUrl: "../partial-views/login.html",
-    controller: function () {
+    controller: function ($auth,$location) {
 
         this.inputType = 'password';
 
         this.login = () => {
-
+             $auth.submitLogin(this.loginForm)
+                   .then(function(resp) {
+                       $location.path("/main");
+                       // handle success response
+                    })
+                    .catch(function(resp) {
+                    // handle error response
+                      $location.path("/");
+                    });
         };
 
         this.showPasswordValue = ()=> {
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
-var zaiprotiv = window.zaiprotiv = angular.module('zaiprotiv', ['ngRoute', 'ngMessages', "autocomplete"]).config(['$routeProvider', function ($routeProvider) {
+var zaiprotiv = window.zaiprotiv = angular.module('zaiprotiv', ['ngRoute', 'ngMessages', "autocomplete",
+ "ng-token-auth"]).config(['$routeProvider', "$authProvider", function ($routeProvider,$authProvider) {
     $routeProvider
         .when('/', {template: '<login></login>'})
         .when('/main', {templateUrl: './partial-views/main.html'})
         .when('/main/subject/:id', {template: '<subject></subject>'})
         .when('/results', {template: '<results></results>'})
         .otherwise({redirectTo: '/'})
+
+        $authProvider.configure({
+            apiUrl: 'http://api.example.com' // real api from Alex 'http://api.example.com'
+        });
 }]);
 
 zaiprotiv.component('subjects', require('./subject/subjects'));
@@ -19,6 +24,8 @@ zaiprotiv.service('dataService', require('./services/dataService'));
 zaiprotiv.component('results', require('./results'));
 
 var autocomplete = require('./autocomplete');
+var cookie = require('./vendor/ng-cookie')
+var token = require('./vendor/ng-token-auth');
 
 module.exports = zaiprotiv;
 
